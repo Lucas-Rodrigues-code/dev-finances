60303c5c763a75e5e227f445742301259c4171cd
lucas <lucas.rodrigues@neocredit.com.br>
2025-06-05 19:39:14 -0300

    feat: Implementa controlador de transações com endpoints para criação, leitura, atualização e exclusão.

---
+++ src/modules/transactions/controllers/transaction_controller.ts
@@ -0,0 +1,49 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+import { TransactionService } from '../services/transaction.service';
+import { CreateTransactionDto, UpdateTransactionDto } from '../dto';
+import { Transaction } from '../entities/transaction.entity';
+
+@Controller('transactions')
+export class TransactionController {
+  constructor(private readonly transactionService: TransactionService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() createUserDto: CreateUserDto): Promise<Transaction> {
+    return this.transactionService.create(CreateTransactionDto);
+  }
+
+  @Get()
+  async findAll(): Promise<Transaction[]> {
+    return this.transactionService.findAll();
+  }
+
+  @Get(':id')
+  async findById(@Param('id') id: string): Promise<Transaction> {
+    return this.transactionService.findById(id);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateTransactionDto,
+  ): Promise<Transaction> {
+    return this.transactionService.update(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async delete(@Param('id') id: string): Promise<void> {
+    return this.transactionService.delete(id);
+  }
+}
