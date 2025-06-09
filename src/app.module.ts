b193d994c911aed62c44d36dcd5ad4207c070263
lucas <lucas.rodrigues@neocredit.com.br>
2025-06-09 20:03:58 -0300

    feat: Atualiza o módulo principal para incluir o módulo de transações e configura o TransactionModule com controlador, serviço e repositório.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { UserModule } from './modules/users/user.module';
 import { databaseConfig } from './config/db';
 import { ConfigModule } from '@nestjs/config';
+//modules
+import { UserModule } from './modules/users/user.module';
+import { TransactionModule } from './modules/transactions/transaction.module';
 
 @Module({
   imports: [
-    
     ConfigModule.forRoot({
       isGlobal: true,
     }),
     TypeOrmModule.forRoot(databaseConfig),
     UserModule,
+    TransactionModule,
   ],
   controllers: [AppController],
   providers: [],
--- src/modules/transactions/transaction.module.ts
+++ src/modules/transactions/transaction.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TransactionController } from './controllers/transaction_controller';
+import { Transaction } from './entities/transaction.entity';
+import { TransactionService } from './services';
+import { TransactionRepository } from './repositories';
 
-@Module({})
-export class TransactionModule {} 
+@Module({
+  imports: [TypeOrmModule.forFeature([Transaction])],
+  controllers: [TransactionController],
+  providers: [TransactionService, TransactionRepository],
+  exports: [TransactionService, TransactionRepository],
+})
+export class TransactionModule {}
