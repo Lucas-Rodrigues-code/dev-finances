704a0212a875a6fa74e027ea06815350b7fd3807
lucas <lucas.rodrigues@neocredit.com.br>
2025-06-21 12:03:21 -0300

    feat: Adiciona configuração do Keycloak e integração com o módulo de autenticação no app.module.ts, além de incluir o docker-compose.yml para serviços de PostgreSQL e Keycloak.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { databaseConfig } from './config/db';
 import { ConfigModule } from '@nestjs/config';
+
+import {
+  KeycloakConnectModule,
+  ResourceGuard,
+  RoleGuard,
+  AuthGuard,
+} from 'nest-keycloak-connect';
+import { APP_GUARD } from '@nestjs/core';
 //modules
 import { UserModule } from './modules/users/user.module';
 import { TransactionModule } from './modules/transactions/transaction.module';
@@ -15,6 +23,14 @@ import { BalanceModule } from './modules/balance/balance.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    KeycloakConnectModule.register({
+      authServerUrl: 'http://localhost:8080', 
+      realm: 'meu-app-realm', 
+      clientId: 'nestjs-app', 
+      secret: 'SEU_CLIENT_SECRET', 
+      cookieKey: 'KEYCLOAK_JWT', 
+      logLevels: ['verbose'], 
+    }),
     TypeOrmModule.forRoot(databaseConfig),
     UserModule,
     TransactionModule,
@@ -22,6 +38,19 @@ import { BalanceModule } from './modules/balance/balance.module';
     BalanceModule,
   ],
   controllers: [AppController],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard, // Protege rotas exigindo autenticação
+    },
+    {
+      provide: APP_GUARD,
+      useClass: ResourceGuard, // Protege rotas baseadas em permissões de recursos
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RoleGuard, // Protege rotas baseadas em roles (papéis)
+    },
+  ],
 })
 export class AppModule {}
